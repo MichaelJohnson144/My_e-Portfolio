99625d760c503a86d87616335823ff58e91a8728
Michael Johnson <michael_johnson144@yahoo.com>
2022-12-31 07:15:24 -0500

    Updated unit tests

--- components/MenuItem/MenuItem.spec.tsx
+++ components/MenuItem/MenuItem.spec.tsx
@@ -10,7 +10,13 @@ describe('MenuItem component', function () {
 
   it("should render the menu's elements' links successfully", function () {
     render(<MenuItem item={{ link: 'test-link', label: 'testLabel' }} />);
-    const link: HTMLLinkElement = screen.getByText('testLabel');
+    const link: HTMLAnchorElement = screen.getByText('testLabel');
     expect(link.href).toBeTruthy();
+  });
+
+  it('should add a target if a href is passed successfully', function () {
+    render(<MenuItem item={{ link: 'test-link', label: 'testLabel', href: '/' }} />);
+    const link: HTMLAnchorElement = screen.getByText('testLabel');
+    expect(link.target).toBe('_blank');
   });
 });
